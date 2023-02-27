f770ffad75e65f03fe97a8c498636233d880acda
RafaelM <rafael.mattos09@gmail.com>
2023-02-27 11:16:35 -0300

    up

--- src/desafio_Conversor_de_Moedas/ConversorMoedas.java
+++ src/desafio_Conversor_de_Moedas/ConversorMoedas.java
@@ -17,6 +17,8 @@ public class ConversorMoedas extends JFrame {
 
 	private JPanel contentPane;
 
+	
+
 	/**
 	 * Launch the application.
 	 */
@@ -24,8 +26,11 @@ public class ConversorMoedas extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+
 					ConversorMoedas frame = new ConversorMoedas();
 					frame.setVisible(true);
+					
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -45,54 +50,56 @@ public class ConversorMoedas extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Converter de Reais a Dólar", "Converter de Reais a Euro", "Converter de Reais a Libras Esterlinas", 
-				"Converter de Reais a Peso Argentino", "Converter de Reais a Peso Chileno", "Converter de Dólar a Reais", "Converter de Euro a Reais", "Converter de Libras Esterlinas a Reais", 
-				"Converter de Peso argentino a  Reais", "Converter de Peso Chileno a Reais"}));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] { "Converter de Reais a Dólar",
+				"Converter de Reais a Euro", "Converter de Reais a Libras Esterlinas",
+				"Converter de Reais a Peso Argentino", "Converter de Reais a Peso Chileno",
+				"Converter de Dólar a Reais", "Converter de Euro a Reais", "Converter de Libras Esterlinas a Reais",
+				"Converter de Peso argentino a  Reais", "Converter de Peso Chileno a Reais" }));
 		comboBox.setBounds(72, 102, 256, 22);
 		contentPane.add(comboBox);
-		
+
 		JButton btnNewButton = new JButton("Ok");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String seleciona = comboBox.getSelectedItem().toString();
-				  if(seleciona == "Converter de Reais a Dólar") {
-					 ConversorController rd = new ConversorController();
-					 rd.Convert(5.2,"Dolar", "maior");
-				  } else if (seleciona == "Converter de Reais a Euro") {
-					  ConversorController re = new ConversorController();
-					  re.Convert(5.5, "Euro", "maior");
-				  } else if (seleciona == "Converter de Reais a Libras Esterlinas" ) {
-					  ConversorController rl = new ConversorController();
-					  rl.Convert(6.2,"Libras", "maior");
-				  } else if (seleciona == "Converter de Reais a Peso Argentino") {
-					  ConversorController rpa = new ConversorController();
-					  rpa.Convert(37.46, "Peso Argentino", "menor");
-				  } else if (seleciona == "Converter de Reais a Peso Chileno") {
-					  ConversorController rpc = new ConversorController();
-					  rpc.Convert(158.61, "Peso Chileno", "menor");
-				  } else if (seleciona == "Converter de Dólar a Reais") {
-					  ConversorController dr = new ConversorController();
-					  dr.Convert(5.2, "Reais", "menor");
-				  } else if (seleciona == "Converter de Euro a Reais") {
-					  ConversorController er = new ConversorController();
-					  er.Convert(5.5, "Reais", "menor");
-				  } else if (seleciona == "Converter de Libras Esterlinas a Reais") {
-					  ConversorController ler = new ConversorController();
-					  ler.Convert(6.2, "Reais", "menor");
-				  } else if (seleciona == "Converter de Peso argentino a  Reais") {
-					  ConversorController par = new ConversorController();
-					  par.Convert(37.46, "Reais", "maior");
-				  } else if (seleciona == "Converter de Peso Chileno a Reais") {
-					  ConversorController pcr = new ConversorController();
-					  pcr.Convert(158.61, "Reais", "maior");
-				  }
+				if (seleciona == "Converter de Reais a Dólar") {
+					ConversorController rd = new ConversorController();
+					rd.Convert(5.2, "Dolar", "maior");
+				} else if (seleciona == "Converter de Reais a Euro") {
+					ConversorController re = new ConversorController();
+					re.Convert(5.5, "Euro", "maior");
+				} else if (seleciona == "Converter de Reais a Libras Esterlinas") {
+					ConversorController rl = new ConversorController();
+					rl.Convert(6.2, "Libras", "maior");
+				} else if (seleciona == "Converter de Reais a Peso Argentino") {
+					ConversorController rpa = new ConversorController();
+					rpa.Convert(37.46, "Peso Argentino", "menor");
+				} else if (seleciona == "Converter de Reais a Peso Chileno") {
+					ConversorController rpc = new ConversorController();
+					rpc.Convert(158.61, "Peso Chileno", "menor");
+				} else if (seleciona == "Converter de Dólar a Reais") {
+					ConversorController dr = new ConversorController();
+					dr.Convert(5.2, "Reais", "menor");
+				} else if (seleciona == "Converter de Euro a Reais") {
+					ConversorController er = new ConversorController();
+					er.Convert(5.5, "Reais", "menor");
+				} else if (seleciona == "Converter de Libras Esterlinas a Reais") {
+					ConversorController ler = new ConversorController();
+					ler.Convert(6.2, "Reais", "menor");
+				} else if (seleciona == "Converter de Peso argentino a  Reais") {
+					ConversorController par = new ConversorController();
+					par.Convert(37.46, "Reais", "maior");
+				} else if (seleciona == "Converter de Peso Chileno a Reais") {
+					ConversorController pcr = new ConversorController();
+					pcr.Convert(158.61, "Reais", "maior");
+				}
 			}
 		});
 		btnNewButton.setBounds(95, 135, 89, 23);
 		contentPane.add(btnNewButton);
-		
+
 		JButton btnNewButton_1 = new JButton("Menu");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -102,9 +109,9 @@ public class ConversorMoedas extends JFrame {
 		});
 		btnNewButton_1.setBounds(213, 135, 89, 23);
 		contentPane.add(btnNewButton_1);
-		
+
 		JLabel lblNewLabel = new JLabel("Escolha uma moeda para converter");
-		lblNewLabel.setBounds(120, 77, 182, 14);
+		lblNewLabel.setBounds(72, 77, 256, 14);
 		contentPane.add(lblNewLabel);
 	}
 
