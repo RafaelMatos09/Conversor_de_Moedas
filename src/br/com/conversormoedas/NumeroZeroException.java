b2ce5fc93d5f08d88c03bc35b41455eee8661f33
RafaelM <rafael.mattos09@gmail.com>
2023-04-02 21:35:17 -0300

    conversor_

--- src/br/com/conversormoedas/NumeroZeroException.java
+++
@@ -1,7 +0,0 @@
-package br.com.conversormoedas;
-
-public class NumeroZeroException extends RuntimeException {
-	public NumeroZeroException() {
-		super("O número não pode ser igual a zero.");
-	}
-}
